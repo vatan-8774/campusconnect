37f6f9c6e84e2bc3264c390b9d1c9efcd18ef660
ninijoe <josephephraim68@yahoo.com>
2023-09-19 15:40:59 -0700

    styled the signup page some more

--- campusconnect/urls.py
+++ campusconnect/urls.py
@@ -16,8 +16,15 @@ Including another URLconf
 # campus_social_network/urls.py
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('social_network.urls')),  # Include app-specific URLs
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

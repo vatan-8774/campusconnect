d525ffb4545f43abec27d900b7720a49068dddec
ninijoe <josephephraim68@yahoo.com>
2023-09-25 01:23:49 -0700

    styled error message for invalid login and signup

--- social_network/views.py
+++ social_network/views.py
@@ -26,7 +26,7 @@ def login_view(request):
         username = request.POST["username"]
         password = request.POST["password"]
         user = authenticate(request, username=username, password=password)
-
+        
         # Check if authentication successful
         if user is not None:
             login(request, user)
